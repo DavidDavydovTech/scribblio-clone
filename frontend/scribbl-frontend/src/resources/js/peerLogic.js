b093e732075257522990830ff5c9c05fc5e02c15
David J. Davydov <davydovjd611@gmail.com>
2020-08-23 10:14:06 -0400

    Populate the Host constructor

--- frontend/scribbl-frontend/src/resources/js/peerLogic.js
+++ frontend/scribbl-frontend/src/resources/js/peerLogic.js
@@ -2,11 +2,17 @@ import Peer from 'peerjs';
 
 // Host code here:
 class Host {
-    constructor (playerObject) {
-        this.verifyPlayerObject(playerObject);
+    constructor (playerObject, rulesObject) {
+        this._peer = new Peer();  
+        this.player = playerObject;
+        this.rules = rulesObject;
 
+        peer.on('open', function(id) {
+            this._id = id;
+        });
     }
 
+    open
     verifyPlayerObject (playerObject) {
         // Example of valid playerObject:
         // let obj = {
@@ -18,7 +24,6 @@ class Host {
         //         body: 'blue-striped'
         //     }
         // }
-
     }
 }
 // Peer code here:
