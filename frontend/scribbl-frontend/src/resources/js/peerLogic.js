5c087bbd330cddf13ee01b78a7b573943372d8ba
David J. Davydov <davydovjd611@gmail.com>
2020-08-23 18:00:24 -0400

    Added a lot of gameRules to Host

--- frontend/scribbl-frontend/src/resources/js/peerLogic.js
+++ frontend/scribbl-frontend/src/resources/js/peerLogic.js
@@ -6,15 +6,40 @@ class Host {
         this._peer = new Peer();  
         this.player = playerObject;
         this.rules = rulesObject;
-        this._gameRunning = true;
-        this._gameTickRate = 33;
-        this._gameTickFunc = () => {
+        this._gameRunning = true; // Used to tell if the server is still running!
+        this._gameTickRate = 60; // How many times per a second the server will 'speak' with players. 
+        this._gameTickFunc = () => { // Function var ran at the end of this.GameTick. Starts off like this and gets populated by another method.
             alert("ERROR: HOST TICK FUNCTION NOT SET");
             this._gameRunning = false;
         };
+        this._gamePlayers = {};
+
+        // All times are in seconds.
+        this._gameRules = {
+            // Time related rules
+            timeDraw: 60, // The maximum amount of time players have to draw/guess a word.
+            timeWait: 10, // The maximum amount of time to wait until picking a random word for the current drawer.
+            timeTransition: 5, // How long to wait between the word-results screen and the next pick screen.
+            timeGrace: 1, // The maximum amount of lenency the host has for laggy players
+            timeWin: 15, // How long to linger on the end-of-game screen.
+            
+            // The ruls will never run because they're set to twice the size of the maximum possible round/draw time (...and also aren't implemented)
+            timeAfkGuesser: 12000, // How long to wait until an inactive guesser gets kicked
+            timeAfkDrawer: 2, // How long (percentage, 0.1 = 10%, 2 = 200%) until an inactive drawer gets kicked durring their round.
+
+            // Game related rules
+            rounds: 3, // Rounds before the winner is declared
+            points: 800, // Points for being the first one to get a word right
+            pointsTimeFalloff: () => { // A function used for calculating how many points players get after adjusting for how much time since round start. Currently unused.
+
+            },
+            pointsPlayerFallloff: () => { //Like above, but removes points based on how many players answered. Currently unusued.
+
+            },
+        };
     }
 
-    initHost ()  {
+    InitHost ()  {
         this._peer.on('open', function(id) {
             this._id = id;
             return id;
@@ -39,8 +64,9 @@ class Host {
     
             this.GameTick();
         }
-
     }
+
+    Game
 }
 // Peer code here:
 
