a0d1a8bd38ab618e3d4b166b013df1e91502b60e
David J. Davydov <davydovjd611@gmail.com>
2020-08-23 11:18:55 -0400

    Add form controls to CreateLobby

--- frontend/scribbl-frontend/src/components/CreateLobby.page.jsx
+++ frontend/scribbl-frontend/src/components/CreateLobby.page.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Card, Button, Form } from 'react-bootstrap';
 class LobbyBrowser extends React.Component {
     constructor () {
         super();
     }
 
-    render () { return (<>
-        <Row>
-            <Col>
-                <h1>Create a Lobby!</h1>
-            </Col>
-        </Row>
-    </> ) }
+    render () { return (<Row>
+        <Col>
+            <h1>Create a Lobby!</h1>
+            <Card>
+                <Card.Body className="align-content-center">
+                    <Form>
+                        <Form.Group controlId="playerInfo">
+                            <Form.Label>Player Name</Form.Label>
+                            <Form.Control type="email" placeholder="SuperMan67" />
+                        </Form.Group>
+                    </Form>
+
+                    <Button>click me</Button>
+                </Card.Body>
+            </Card>
+        </Col>
+    </Row>) }
 }
 
 export default LobbyBrowser;
