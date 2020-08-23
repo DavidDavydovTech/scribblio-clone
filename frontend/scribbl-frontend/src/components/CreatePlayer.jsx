3be579598e0895a8f66a6abde5b0b99603964c87
David J. Davydov <davydovjd611@gmail.com>
2020-08-23 13:12:16 -0400

    Make CreatePlayer actually return a player name

--- frontend/scribbl-frontend/src/components/CreatePlayer.jsx
+++ frontend/scribbl-frontend/src/components/CreatePlayer.jsx
@@ -5,12 +5,27 @@ class CreatePlayer extends React.Component {
         super(props);
 
         this.state = {
-            name: "",
+            refs: {
+                name: React.createRef()
+            },
         }
     }
 
     prepareLobby = () => {
-        this.props.onSubmit(this.state.name);
+        let name = this.state.refs.name.current.value;
+        switch (true) {
+            case typeof name === 'number':
+                name.toString();
+            case typeof name !== 'string':
+                name = 'undefined';
+                console.warn('Warning: Name was not a string. Assigning player a name of "undefined" instead.');
+            case name.length < 3:
+                // TODO: Add error handling for this form.
+            case name.length > 12:
+                // TODO: Add error handling for this form.
+            // TODO: Add a check to see if user has a curse word in their name.
+        }
+        this.props.onSubmit(name);
     }   
 
     render () { return (
@@ -19,7 +34,7 @@ class CreatePlayer extends React.Component {
                 <Form>
                     <Form.Group controlId="playerInfo">
                         <Form.Label>Player Name</Form.Label>
-                        <Form.Control type="username" placeholder="SuperMan67" />
+                        <Form.Control ref={this.state.refs.name} type="username" placeholder="SuperMan67" />
                     </Form.Group>
                 </Form>
 
