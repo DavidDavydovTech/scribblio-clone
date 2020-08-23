815f86f50d6aae892ff281ec841f2f29084e18ea
David J. Davydov <davydovjd611@gmail.com>
2020-08-23 10:41:15 -0400

    Added reactRouter template to App

--- frontend/scribbl-frontend/src/App.js
+++ frontend/scribbl-frontend/src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
+
 import './App.css';
 import './resources/css/bootstrap.min.css';
 
@@ -8,9 +15,32 @@ import Game from './components/Game.page';
 
 function App() {
   return (
-    <div className="App">
-      <Game/>
-    </div>
+    <Router>
+      <div>
+        <h2>Accounts</h2>
+
+        <ul>
+          <li>
+            <Link to="/netflix">Netflix</Link>
+          </li>
+          <li>
+            <Link to="/zillow-group">Zillow Group</Link>
+          </li>
+          <li>
+            <Link to="/yahoo">Yahoo</Link>
+          </li>
+          <li>
+            <Link to="/modus-create">Modus Create</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/:id">
+            <Game/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
