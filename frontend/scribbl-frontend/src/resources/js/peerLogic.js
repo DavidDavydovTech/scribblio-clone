c9800de044238db01d7079629064764351c403cc
David J. Davydov <davydovjd611@gmail.com>
2020-08-23 10:06:50 -0400

    Add verifyPlayerObject to Host

--- frontend/scribbl-frontend/src/resources/js/peerLogic.js
+++ frontend/scribbl-frontend/src/resources/js/peerLogic.js
@@ -1,5 +1,24 @@
 import Peer from 'peerjs';
 
 // Host code here:
+class Host {
+    constructor (playerObject) {
+        this.verifyPlayerObject(playerObject);
 
+    }
+
+    verifyPlayerObject (playerObject) {
+        // Example of valid playerObject:
+        // let obj = {
+        //     id: 121204812048124091240,
+        //     name: "American Idi",
+        //     avatar: {
+        //         eyes: 'suspicious',
+        //         mouth: 'happy-v',
+        //         body: 'blue-striped'
+        //     }
+        // }
+
+    }
+}
 // Peer code here:
