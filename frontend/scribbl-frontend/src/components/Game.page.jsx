3d48e55871a941f0258e2938748e47de528f5443
David J. Davydov <davydovjd611@gmail.com>
2020-08-23 21:02:17 -0400

    Make canvas functional,

--- frontend/scribbl-frontend/src/components/Game.page.jsx
+++ frontend/scribbl-frontend/src/components/Game.page.jsx
@@ -16,10 +16,23 @@ class Game extends React.Component {
                 "28c641",
                 "2d93dd",
                 "7b53ad",
-            ]
+            ],
+            canvasSize: {
+                x: window.innerWidth*0.5,
+                y: window.innerWidth*0.375
+            }
         }
+
+        window.addEventListener('resize', () => {
+            this.setState({
+                canvasSize: {
+                    x: window.innerWidth*0.5,
+                    y: window.innerWidth*0.375
+                }})
+        });
     }
 
+    
     componentDidMount () {
         this.initalizeCanvas();
     }
@@ -31,11 +44,46 @@ class Game extends React.Component {
         this.ctx.fillStyle = '#ffffff'
         this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height)
 
-        let test = new Host({name: "david", avatar: {}})
+        let isDrawing = false;
+        let points = [];
+        this.ctx.strokeStyle = "#1b1c33";
+        this.ctx.fillStyle = '#ffffff'
+        
+        this.el.onmousedown = (e) => {
+            isDrawing = true;
+            points.push({ x: e.clientX, y: e.clientY });
+            console.log("down")
+        }
 
-        // this.el.mouseDown(() => {
-        //     alert("k")
-        // })
+        this.el.onmousemove = (e) => {
+            if (isDrawing) {
+                points.push({ x: e.clientX, y: e.clientY });
+
+                let boundingBox = this.el.getBoundingClientRect();
+                this.ctx.beginPath();
+                points.forEach((value, i) => {
+                    if (i === 0) {
+                        this.ctx.moveTo(
+                            value.x - boundingBox.x, 
+                            value.y - boundingBox.y, 
+                        );
+                    } else {
+                        this.ctx.lineTo(
+                            value.x - boundingBox.x, 
+                            value.y - boundingBox.y, 
+                        );
+                    }
+                })
+                this.ctx.stroke();
+                
+            }
+        }
+
+        this.el.onmouseup = (e) => {
+            isDrawing = false;
+            points = [];
+        }
+        // let test = new Host({name: "david", avatar: {}})
     }
 
     // enableDrawing () {
@@ -54,9 +102,16 @@ class Game extends React.Component {
                     <b>_ i s _</b>
                     <i>Time Left: 13s</i>
                 </Card.Header>
+                
                 <Card.Body className="p-0">
-                    <canvas ref={this.state.canvasRef} className="white-board"></canvas>
+                    <canvas 
+                    ref={this.state.canvasRef} 
+                    className="white-board"
+                    width={this.state.canvasSize.x} 
+                    height={this.state.canvasSize.y}
+                    ></canvas>
                 </Card.Body>
+                
                 <Card.Footer className="d-flex w-100 align-self-center justify-content-center">
                     <Col xs={"auto"}>
                         <ButtonGroup>
